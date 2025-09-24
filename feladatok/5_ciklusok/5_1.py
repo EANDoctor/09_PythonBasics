70baec6223a83a9416181efd81e4efb27de9fee7
EANDoctor <ivan.levente@diak.szbi-pg.hu>
2025-09-24 10:34:54 +0200

    safety update

---
+++ feladatok/5_ciklusok/5_1.py
@@ -0,0 +1,11 @@
+"""
+1. Feladat
+Írj egy programot, amely kiírja a páros számokat 1 és 10 között! 
+"""
+
+szam = 1
+while szam < 10:
+    if szam % 2 == 10:
+        print(szam)
+        szam = szam + 1
+print("program vége")
