34cfde562b4cf7a65f6249e6563e6e3ce96161a8
EANDoctor <ivan.levente@diak.szbi-pg.hu>
2025-09-24 08:17:42 +0200

    Create Ivan_Levente_09_14_dolgozat.py

---
+++ Ivan_Levente_09_14_dolgozat.py
@@ -0,0 +1,20 @@
+"""
+B csoport:
+Készítsünk programot, amely segíti a burkoló mesterek munkáját.
+A szükséges csempe mennyiségének a kiszámításához a program kérje be a terület szélességét és a magasságát centiméterben,
+valamint, hogy hány darab csempét vásároltunk már, majd számolja ki a területét (a*b), és
+hogy a 20cm*20cm méretű csempék esetén hány darabra van szükség a munka elvégzéséhez (a plusz 10%-ot az illesztések miatt illik rászámolnunk).
+A program azt is állapítsa meg, és közölje a felhasználóval, hogy kell-e még vásárolnunk csempét!
+"""
+
+a = int(input("Add meg a terület szélességét cm-ben:"))
+b = int(input("Add meg a terület magasságát cm-ben:"))
+terület = a * b 
+csempe_területe = 20 * 20
+csempe_mennyiség = int(input("Mennyi csempénk van?"))
+csempe_szükséglet = (terület / csempe_területe) * 1.1
+
+if csempe_szükséglet >= terület:
+    print("baj van kell még csempe.")
+else:
+    print("Minden rendben több csempénk van.")
